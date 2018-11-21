45ab97f2ecd9562a19f78c6aa579158acf278272
eecheng <yucheng871011@gmail.com>
2018-11-21 18:07:38 +0800

    add Move feature

--- game.js
+++ game.js
@@ -1,60 +1,43 @@
 let player = document.getElementById('player');
-
+let gamezone = document.getElementById('gameZone');
 player.style.left = 50 + 'px';
 player.style.top = 150 + 'px';
 //player.src = "../resources/speedPig.png";
 let x, y;
-let speed = 10;
+let speed = 1;
 
-let map = { 65: false, 83: false, 87: false, 68: false };
+let gamezone_left = (window.innerWidth - 400) / 2;
 
-window.addEventListener('keydown', (e) => {
-    if (e.keyCode in map) { //key is exist in map object
-        map[e.keyCode] = true;
-    }
-    //handle different combination of key and move player
-    if (map[68] && map[87]) {
-        player.style.left = parseInt(player.style.left) + speed + 'px';
-        player.style.top = parseInt(player.style.top) - speed + 'px';
+let player_centerX = parseInt(player.style.left) + 50;
+let player_centerY = parseInt(player.style.top) + 50;
+
+let mouseX, mouseY;
+let distance;
+
+setInterval(update, 10);
+
+player.src = "../resources/strengthPig.png";
+gamezone.addEventListener('mousemove', (e) => {
+
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    player_centerX = parseInt(player.style.left) + gamezone_left + 25;
+    player_centerY = parseInt(player.style.top) + 25;
+    distance = Math.sqrt((mouseX - player_centerX) * (mouseX - player_centerX) + (mouseY - player_centerY) * (mouseY - player_centerY));
+    //console.log(distance);
+    //console.log(gamezone_left + parseInt(player.style.left));
+    //console.log(e.clientY);
+    //console.log(parseInt(player.style.top));
+})
+
+function update() {
+
+    if (Math.abs(mouseX - parseFloat(player.style.left) - gamezone_left - 25) < 2 && Math.abs(mouseY - parseFloat(player.style.top) - 25) < 2)
         return;
-    }
-    if (map[68] && map[83]) {
-        player.style.left = parseInt(player.style.left) + speed + 'px';
-        player.style.top = parseInt(player.style.top) + speed + 'px';
-        return;
-    }
-    if (map[65] && map[87]) {
-        player.style.left = parseInt(player.style.left) - speed + 'px';
-        player.style.top = parseInt(player.style.top) - speed + 'px';
-        return;
-    }
-    if (map[65] && map[83]) {
-        player.style.left = parseInt(player.style.left) - speed + 'px';
-        player.style.top = parseInt(player.style.top) + speed + 'px';
-        return;
-    }
-    if (map[68]) { //move to right
-        player.style.left = parseInt(player.style.left) + speed + 'px';
-        return;
-    }
-    if (map[65]) {
-        player.style.left = parseInt(player.style.left) - speed + 'px';
-        return;
-    }
-    if (map[87]) { //up
-        player.style.top = parseInt(player.style.top) - speed + 'px';
-        return;
-    }
-    if (map[83]) {
-        player.style.top = parseInt(player.style.top) + speed + 'px';
-        return;
-    }
+
+    console.log(parseFloat(player.style.left) + speed * (mouseX - player_centerX) / distance + 'px');
+    player.style.left = parseFloat(player.style.left) + speed * (mouseX - player_centerX) / distance + 'px';
+    player.style.top = parseFloat(player.style.top) + speed * (mouseY - player_centerY) / distance + 'px';
 
 
-});
-
-window.addEventListener('keyup', (e) => {
-    if (map[e.keyCode]) {
-        map[e.keyCode] = false;
-    }
-});
+}
